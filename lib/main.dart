1bbdf8be8b2b890948b5f41585b28d89aa9d727e
WilliamRueda <william.rueda@correounivalle.edu.co>
2024-02-07 15:35:52 -0500

    login

--- lib/main.dart
+++ lib/main.dart
@@ -28,18 +28,73 @@ class _InicioState extends State<Inicio> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-        backgroundColor: Colors.deepOrange,
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          Text(widget.title),
-          Text("Fleyarin"),
-        ],
-      ),
+      body: cuerpo(),
     );
   }
+}
+
+Widget cuerpo() {
+  return Container(
+    decoration: BoxDecoration(
+      image: DecorationImage(
+          image: AssetImage('assets/images/1.png'), fit: BoxFit.cover),
+    ),
+    child: Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          nombre(),
+          campoUsuario(),
+          campoContrasena(),
+          SizedBox(height: 10),
+          botonEntrar()
+        ],
+      ),
+    ),
+  );
+}
+
+Widget nombre() {
+  return Text(
+    "Sign in",
+    style: TextStyle(
+        color: Colors.white, fontSize: 35.0, fontWeight: FontWeight.bold),
+  );
+}
+
+Widget campoUsuario() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 17, vertical: 5),
+    child: TextField(
+      decoration: InputDecoration(
+        hintText: "User",
+        fillColor: Colors.white60,
+        filled: true,
+      ),
+    ),
+  );
+}
+
+Widget campoContrasena() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 17, vertical: 5),
+    child: TextField(
+      obscureText: true,
+      decoration: InputDecoration(
+        hintText: "Password",
+        fillColor: Colors.white60,
+        filled: true,
+      ),
+    ),
+  );
+}
+
+Widget botonEntrar() {
+  return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+          backgroundColor: Colors.greenAccent,
+          foregroundColor: Colors.black,
+          padding: EdgeInsets.symmetric(horizontal: 50, vertical: 10)),
+      onPressed: () {},
+      child: Text("Log in"));
 }
